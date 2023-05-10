6c26e3be4bf3ef8798228b2a702f286475d1861e
Syed Sanzid Hasan <syed.hasan@nagad.com.bd>
2023-05-10 18:39:38 +0600

    - Changed SendHTTPRequest to be more dynamic. Can now set method, timeout and headers when called.

--- src/main/java/com/billercode/api/billercodeapi/controllers/AirlineController.java
+++ src/main/java/com/billercode/api/billercodeapi/controllers/AirlineController.java
@@ -1,6 +1,7 @@
 package com.billercode.api.billercodeapi.controllers;
 
 import com.billercode.api.billercodeapi.models.RequestJson;
+import com.billercode.api.billercodeapi.utils.SendHTTPRequest;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,10 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +22,6 @@ import java.util.Map;
 @RequestMapping("/airlines")
 public class AirlineController {
 
-    private final RestTemplate restTemplate = new RestTemplate();
     @Autowired
     private Gson gson;
 
@@ -31,8 +30,10 @@ public class AirlineController {
         try {
 
             HashMap<String, String> apiList = new HashMap<>();
+            Map<String, String> headers = new HashMap<>();
             URL endpointUrl;
-            ResponseEntity<String> response = null;
+            ResponseEntity<String> response;
+
             Map<String, Object> requestPayload = new HashMap<>();
             apiList.put("1101", "https://api.instantwebtools.net/v1/airlines");
             apiList.put("1102", "https://api.instantwebtools.net/v1/passenger");
@@ -43,10 +44,14 @@ public class AirlineController {
                 return ResponseEntity.badRequest().body("Biller code cannot be null");
             }
 
+            String responseBody;
+
             switch (billerCode) {
-                case "1101": // add airlines
+                case "1101" -> { // add airlines
                     endpointUrl = new URL(apiList.get(billerCode));
 
+                    // setting the headers to use for the request
+                    headers.put("Content-Type", "application/json");
                     requestPayload.put("name", request.getParam1());
                     requestPayload.put("country", request.getParam2());
                     requestPayload.put("logo", request.getParam3());
@@ -55,27 +60,39 @@ public class AirlineController {
                     requestPayload.put("website", request.getParam6());
                     requestPayload.put("established", request.getParam7());
 
-                    response = ResponseEntity.ok().body(restTemplate.postForObject(endpointUrl.toURI(), requestPayload, String.class));
-                    break;
-                case "1102": // add passenger
-                    endpointUrl = new URL(apiList.get(billerCode));
+                    //Using HttpUrlConnection
+                    try {
+                        // sending the request
+                        responseBody = SendHTTPRequest.sendHttpRequest("POST",10000, requestPayload, endpointUrl, headers);
 
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    response = ResponseEntity.ok().body(responseBody);
+                }
+                case "1102" -> { // add passenger
+                    endpointUrl = new URL(apiList.get(billerCode));
                     requestPayload.put("name", request.getParam1());
                     requestPayload.put("trips", request.getParam2());
                     requestPayload.put("airline", request.getParam3());
-
-                    response = ResponseEntity.ok().body(restTemplate.postForObject(endpointUrl.toURI(), requestPayload, String.class));
-                    break;
-
-                default: // incase code not in use here
+                    try {
+                        responseBody = SendHTTPRequest.sendHttpRequest("POST",10000, requestPayload, endpointUrl, headers);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    response = ResponseEntity.ok().body(responseBody);
+                }
+                //response = ResponseEntity.ok().body(restTemplate.postForObject(endpointUrl.toURI(), requestPayload, String.class));
+                default -> { // incase code not in use here
                     requestPayload.put("Message", "The code sent is not valid for any vendors");
                     response = ResponseEntity.badRequest().body(gson.toJson(requestPayload));
+                }
             }
 
 
             return response;
 
-        } catch (MalformedURLException | URISyntaxException e) {
+        } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
---
+++ src/main/java/com/billercode/api/billercodeapi/utils/SendHTTPRequest.java
@@ -0,0 +1,64 @@
+package com.billercode.api.billercodeapi.utils;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+
+public class SendHTTPRequest {
+
+
+    static Gson gson = new Gson();
+    public static String sendHttpRequest(String requestMethod, int timeout, Map<String, Object> requestPayload, URL endpointUrl, Map<String,String> headers) throws IOException {
+        BufferedReader streamReader;
+        StringBuilder fullResponseBuilder =  new StringBuilder();
+
+        HashSet<String> requestMethods = new HashSet<>(Arrays.asList( "GET", "POST", "PUT", "DELETE"));
+
+        HttpURLConnection connection =  (HttpURLConnection) endpointUrl.openConnection();
+
+        if (requestMethods.contains(requestMethod) ){
+            connection.setRequestMethod(requestMethod.toUpperCase());
+
+            for (Map.Entry<String, String> entry : headers.entrySet()){
+                connection.setRequestProperty(entry.getKey(),entry.getValue());
+            }
+
+            //connection.setRequestProperty("Accept","application/json"); Not required most of the time
+            connection.setConnectTimeout(timeout);
+            connection.setReadTimeout(timeout);
+            if (!requestMethod.equalsIgnoreCase("GET")){
+                connection.setDoOutput(true);
+                String jsonString = gson.toJson(requestPayload);
+                OutputStream outStream = connection.getOutputStream();
+                byte[] jsonBody = jsonString.getBytes(StandardCharsets.UTF_8);
+                outStream.write(jsonBody,0,jsonBody.length);
+            }
+            else{
+                connection.setDoOutput(false);
+            }
+
+            int status = connection.getResponseCode();
+            if (status > 299) {
+                streamReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            } else {
+                streamReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            }
+
+            return fullResponseBuilder.append(streamReader.readLine()).toString();
+
+        }
+        else{
+            throw new RuntimeException("Error! Request method is incorrect");
+        }
+
+    }
+}
